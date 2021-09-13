d011bd306c5fb24c1eafcc5846801c1495a7915f
pvttrung <64237915+pvttrung@users.noreply.github.com>
2021-09-13 17:23:33 +0300

    Add files via upload
    cpp file for new problem

---
+++ OPPOSITE.cpp
@@ -0,0 +1,43 @@
+#include<fstream>
+#include<stdlib.h>
+using namespace std;
+
+int oppo(int i,int j,int k)
+{
+	int half;
+	half = abs(i-j);
+	if(k > half*2 || (i > j && j-1 > half-1) || (i < j && i-1 > half-1) || half <=1) 
+	{
+		return -1;
+	} else
+	{
+		if(k > half)
+		{	
+			return k-half;
+		} else
+		{	
+			return k+half;
+		}
+	}
+}
+
+int main()
+{
+	int n,i,a,b,c,re;	
+	ifstream inp;
+	ofstream outp;
+	inp.open("OPPOSITE.INP");
+	outp.open("OPPOSITE.OUT");
+	
+	inp >> n;
+	for(i=1;i<=n;i++)
+	{
+		inp >> a >> b >> c;
+		re = oppo(a,b,c);
+		outp << re << endl;
+	}
+
+	inp.close();
+	outp.close();
+	return 0;
+}
